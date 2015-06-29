c32b17b39dd486bcd93fa7a347b5504fa7ebab1c
chriso <cro3@njit.edu>
2015-06-28 20:36:20 -0400

    Added code to enable addition of mouse listener to result list in search panel

--- src/edu/njit/cs/saboc/nat/generic/gui/panels/SearchPanel.java
+++ src/edu/njit/cs/saboc/nat/generic/gui/panels/SearchPanel.java
@@ -20,6 +20,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -185,6 +186,10 @@ public class SearchPanel<T> extends JPanel {
         searchList.setFilterPanelOpen(false, null);
 
         this.add(searchList, c);
+    }
+    
+    public void addResultListMouseListener(MouseListener listener) {
+        searchList.addListMouseListener(listener);
     }
     
     private JRadioButton makeRadioButton(String text, ButtonGroup group, Container panel) {

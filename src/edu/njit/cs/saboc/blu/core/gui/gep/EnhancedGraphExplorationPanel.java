e3129a27e9116c90de74ad18acf562b578e11541
chriso <cro3@njit.edu>
2015-07-20 14:31:06 -0400

    Merge origin/master into RevisedAbNGUI
    Conflicts:
    	src/edu/njit/cs/saboc/blu/core/gui/gep/EnhancedGraphExplorationPanel.java

--- src/edu/njit/cs/saboc/blu/core/gui/gep/EnhancedGraphExplorationPanel.java
+++ src/edu/njit/cs/saboc/blu/core/gui/gep/EnhancedGraphExplorationPanel.java
@@ -291,8 +291,6 @@ public class EnhancedGraphExplorationPanel extends JPanel {
                             }
                         } else {
                             selectionStateMonitor.resetAll();
-                            
-                            partitionOptionsPanel.doHide();
                         }
                     }
                     

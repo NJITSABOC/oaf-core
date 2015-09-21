a4d32d4dfb193300f99445a6705110c2823d7974
chriso <cro3@njit.edu>
2015-09-21 19:23:07 -0400

    Moved fitler functionality into abstract entity list

--- src/edu/njit/cs/saboc/blu/core/gui/gep/panels/details/AbstractEntityList.java
+++ src/edu/njit/cs/saboc/blu/core/gui/gep/panels/details/AbstractEntityList.java
@@ -2,37 +2,60 @@ package edu.njit.cs.saboc.blu.core.gui.gep.panels.details;
 
 import edu.njit.cs.saboc.blu.core.gui.gep.panels.details.listeners.EntitySelectionListener;
 import edu.njit.cs.saboc.blu.core.gui.gep.panels.details.models.BLUAbstractTableModel;
+import edu.njit.cs.saboc.blu.core.gui.iconmanager.IconManager;
 import edu.njit.cs.saboc.blu.core.gui.utils.renderers.MultiLineTextRenderer;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.Optional;
+import java.util.regex.PatternSyntaxException;
 import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.RowFilter;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 /**
  *
  * @author Chris O
  */
 public abstract class AbstractEntityList<T> extends JPanel {
+
     protected final JTable entityTable;
+    protected final BLUAbstractTableModel<T> tableModel;
     
-    protected BLUAbstractTableModel<T> tableModel = null;
+    protected final ArrayList<EntitySelectionListener<T>> selectionListeners = new ArrayList<>();
     
-    protected ArrayList<EntitySelectionListener<T>> selectionListeners = new ArrayList<>();
+    private final JTextField filterField = new JTextField();
+    private final JButton closeButton = new JButton();
+
+    private final TableRowSorter<TableModel> sorter;
     
-    private final JPanel optionsPanel;
+    private final JPanel filterPanel;
+    
+    //private final JPanel optionsPanel;
     
     protected AbstractEntityList(BLUAbstractTableModel<T> tableModel) {
         super(new BorderLayout());
 
-        this.entityTable = new JTable(this.tableModel = tableModel);
+        this.tableModel = tableModel;
+        this.entityTable = new JTable(tableModel);
         this.entityTable.setFont(entityTable.getFont().deriveFont(Font.PLAIN, 14));
         this.entityTable.setDefaultRenderer(String.class, new MultiLineTextRenderer());
         
@@ -61,10 +84,131 @@ public abstract class AbstractEntityList<T> extends JPanel {
         
         this.add(new JScrollPane(entityTable), BorderLayout.CENTER);
         
+        ///////////////////////////
+        //Container for filtering//
+        ///////////////////////////
+        closeButton.setIcon(IconManager.getIconManager().getIcon("cross.png"));
+        closeButton.setToolTipText("Close");
+        
+        filterPanel = new JPanel();
+        filterPanel.setLayout(new BoxLayout(filterPanel, BoxLayout.X_AXIS));
+        filterPanel.add(closeButton);
+        filterPanel.add(Box.createHorizontalStrut(10));
+        filterPanel.add(new JLabel("Filter:  "));
+        filterPanel.add(filterField);
+        filterPanel.setVisible(false);
+
+        this.add(filterPanel, BorderLayout.SOUTH);
+
+        ///////////////////////////
+        //End Filtering Container//
+        //  Start Sorter Model   //
+        ///////////////////////////
+        sorter = new TableRowSorter<TableModel>(tableModel);
+        
+        entityTable.setRowSorter(sorter);
+
+        /////////////////////////
+        //Filters for Listening//
+        /////////////////////////
+        filterField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent ke) {
+                if (ke.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    filterField.setText("");
+                    
+                    setFilterPanelOpen(false, null);
+                    
+                    entityTable.requestFocus();
+                }
+            }
+        });
+
+        closeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setFilterPanelOpen(false, null);
+            }
+        });
+
+        filterField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                newFilter();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                newFilter();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                newFilter();
+            }
+        });
+
+        entityTable.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+
+                if (!e.isControlDown() && !e.isAltDown()) {
+                    if (!filterPanel.isVisible()) {
+                        setFilterPanelOpen(true, e);
+                    } else {
+                        filterField.requestFocus();
+                        filterField.setText(filterField.getText() + e.getKeyChar());
+                    }
+                }
+            }
+        });
+        
+        /*
         optionsPanel = new JPanel();
         this.add(optionsPanel, BorderLayout.SOUTH);
+        */
         
         setBorderText(getBorderText(Optional.empty()));
+    }
+    
+    private void newFilter() {
+
+        RowFilter<TableModel, Object> rf = null;
+        
+        try {
+            rf = RowFilter.regexFilter("(?i)" + filterField.getText());
+        } catch (PatternSyntaxException e) {
+            return;
+        }
+        
+        sorter.setRowFilter(rf);
+    }
+    
+    /* opens (open = true) or closes the filter panel */
+    public void toggleFilterPanel() {
+        if (!filterPanel.isVisible()) {
+            setFilterPanelOpen(true, null);
+        } else {
+            setFilterPanelOpen(false, null);
+        }
+    }
+
+    /*opens the filter panell and uses a KeyEvent if openned by typing */
+    public void setFilterPanelOpen(boolean open, KeyEvent e) {
+        if (open) {
+            if (!filterPanel.isVisible()) {
+                filterPanel.setVisible(true);
+
+                if (e != null) {
+                    filterField.setText(Character.toString(e.getKeyChar()));
+                } else {
+                    filterField.setText("");
+                }
+
+                filterField.requestFocus();
+            }
+        } else {
+            filterPanel.setVisible(false);
+        }
     }
     
     public void addEntitySelectionListener(EntitySelectionListener<T> listener) {
@@ -86,7 +230,7 @@ public abstract class AbstractEntityList<T> extends JPanel {
     }
     
     protected void addOptionButton(JButton btn) {
-        optionsPanel.add(btn);
+        //optionsPanel.add(btn);
     }
 
     protected abstract String getBorderText(Optional<ArrayList<T>> entities);
--- src/edu/njit/cs/saboc/blue/core/utils/filterable/tablePanel/FilterableTablePanel.java
+++ src/edu/njit/cs/saboc/blue/core/utils/filterable/tablePanel/FilterableTablePanel.java
@@ -6,25 +6,13 @@
 package edu.njit.cs.saboc.blue.core.utils.filterable.tablePanel;
 
 import edu.njit.cs.saboc.blu.core.gui.iconmanager.IconManager;
-import edu.njit.cs.saboc.blu.core.utils.filterable.list.Filterable;
-import edu.njit.cs.saboc.blu.core.utils.filterable.list.FilterableListModel;
 import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Font;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
-import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -46,37 +34,29 @@ import javax.swing.table.TableRowSorter;
  * @author Vladimir V
  */
 public class FilterableTablePanel extends JPanel {
+
+    private final JTextField filterField = new JTextField();
+    private final JButton closeButton = new JButton();
+
+    private TableRowSorter<TableModel> sorter;
     
-    private JTextField filterField = new JTextField();
-    private JButton closeButton = new JButton();
-    TableRowSorter<TableModel> sorter;
-//    private DefaultListModel pleaseWaitModel = new DefaultListModel();
-//    private DefaultListModel dataEmptyModel = new DefaultListModel();
-//    
-    //protected FilterableListModel entryModel;
-    
-    protected JList list;
     private JPanel filterPanel = new JPanel();
 
     public FilterableTablePanel() {
-        this.setLayout(new BorderLayout());        
+        this.setLayout(new BorderLayout());
     }
 
-    public void add(JScrollPane comp)
-    {
+    public void add(JScrollPane comp) {
         add(comp, null);
     }
+
     public void add(JScrollPane comp, Object constraints) {
         super.add(comp, constraints);
-        
-//
-//        pleaseWaitModel.addElement("Please wait...");
-//        dataEmptyModel.addElement(" ");
 
-        
         JViewport viewport = comp.getViewport();
-       
-        JTable table = (JTable)viewport.getView();
+
+        JTable table = (JTable) viewport.getView();
+
         ///////////////////////////
         //Container for filtering//
         ///////////////////////////
@@ -91,36 +71,40 @@ public class FilterableTablePanel extends JPanel {
         filterPanel.setVisible(false);
 
         this.add(filterPanel, BorderLayout.SOUTH);
+
         ///////////////////////////
         //End Filtering Container//
         //  Start Sorter Model   //
         ///////////////////////////
-        
         sorter = new TableRowSorter<TableModel>(table.getModel());
+        
         table.setRowSorter(sorter);
         table.getSelectionModel().addListSelectionListener(
-            new ListSelectionListener() {
-                public void valueChanged(ListSelectionEvent e) {
-                    int viewRow = table.getSelectedRow();
-                    if (viewRow >= 0) {
-                        //Selection did not got filtered away.
-                        int modelRow = table.convertRowIndexToModel(viewRow);
+                new ListSelectionListener() {
+                    public void valueChanged(ListSelectionEvent e) {
+                        int viewRow = table.getSelectedRow();
+                        
+                        if (viewRow >= 0) {
+                            
+                            //Selection did not got filtered away.
+                            int modelRow = table.convertRowIndexToModel(viewRow);
+                            
+                            
+                        }
                     }
-                }
-            });
-        
-        
+                });
+
         /////////////////////////
         //Filters for Listening//
         /////////////////////////
         filterField.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent ke) {
-                if(ke.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                if (ke.getKeyCode() == KeyEvent.VK_ESCAPE) {
                     filterField.setText("");
                     setFilterPanelOpen(false, null);
                     table.requestFocus();
-                    
+
                 }
             }
         });
@@ -148,134 +132,63 @@ public class FilterableTablePanel extends JPanel {
                 newFilter();
             }
 
-            
         });
-        
 
         table.addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent e) {
 
                 if (!e.isControlDown() && !e.isAltDown()) {
-                    if(!filterPanel.isVisible())
+                    if (!filterPanel.isVisible()) {
                         setFilterPanelOpen(true, e);
-                    else {
+                    } else {
                         filterField.requestFocus();
                         filterField.setText(filterField.getText() + e.getKeyChar());
-                        
                     }
                 }
             }
         });
-        /////////////////
-        //End Listeners//
-        /////////////////
-            
-        
-        
-    
     }
-    
+
     private void newFilter() {
-        
+
         RowFilter<TableModel, Object> rf = null;
+        
         try {
             rf = RowFilter.regexFilter("(?i)" + filterField.getText());
-        }   catch (java.util.regex.PatternSyntaxException e) {
+        } catch (java.util.regex.PatternSyntaxException e) {
+            
             System.out.println(e.toString());
             return;
         }
+        
         sorter.setRowFilter(rf);
-    }
-    /*
-    public void updateTable(String text, JTable table) {
-        text = text.toLowerCase();
-        if(text.isEmpty()) {
-//            (table.getModel())
-        }
-    }
-    public void showPleaseWait() {
-        list.setModel(pleaseWaitModel);
-//        entryModel.changeFilter("");
-        filterPanel.setVisible(false);
-    }
-
-    public void showDataEmpty() {
-        list.setModel(dataEmptyModel);
-//        entryModel.changeFilter("");
-        filterPanel.setVisible(false);
-    }
-
-    public void setContents(Collection<? extends Filterable> content) {
-//        entryModel.changeFilter("");
-//        filterPanel.setVisible(false);
-//        entryModel.clear();
-//        entryModel.addAll(content);
-//        list.setModel(entryModel);
     }
 
     /* opens (open = true) or closes the filter panel */
     public void toggleFilterPanel() {
-        if(!filterPanel.isVisible()) {
+        if (!filterPanel.isVisible()) {
             setFilterPanelOpen(true, null);
-        }
-        else {
+        } else {
             setFilterPanelOpen(false, null);
         }
     }
 
     /*opens the filter panell and uses a KeyEvent if openned by typing */
     public void setFilterPanelOpen(boolean open, KeyEvent e) {
-        if(open) {
-            if(!filterPanel.isVisible()) {
+        if (open) {
+            if (!filterPanel.isVisible()) {
                 filterPanel.setVisible(true);
-                
-                if(e != null) {
+
+                if (e != null) {
                     filterField.setText(Character.toString(e.getKeyChar()));
                 } else {
                     filterField.setText("");
                 }
-                
+
                 filterField.requestFocus();
             }
         } else {
-            //entryModel.changeFilter("");
             filterPanel.setVisible(false);
-            //list.grabFocus();
         }
     }
-    
-    /*public void addListMouseListener(MouseListener listener) {
-        list.addMouseListener(listener);
-    }
-    
-    public void removeListMouseListener(MouseListener listener) {
-        list.removeMouseListener(listener);
-    }
-    
-    public List<Filterable> getSelectedValues() {
-        
-        if(list.getSelectedIndices().length == 0) {
-            return (List<Filterable>)Collections.EMPTY_LIST;
-        }
-        
-        ArrayList<Filterable> selectedItems = new ArrayList<Filterable>();
-        
-        int [] selectedIndices = list.getSelectedIndices();
-        
-        for(int index : selectedIndices) {
-//            selectedItems.add(entryModel.getFilterableAtModelIndex(index));
-        }
-
-        return selectedItems;
-    }
-    
-    public int getSelectedIndex() {
-        return list.getSelectedIndex();
-    }
-    
-    public void setListFontSize(int size) {
-        if(size > 0) {
-            list.setFont(list.getFont().deriveFont(Font.PLAIN, (float)size));
-        }
-    }*/
 }

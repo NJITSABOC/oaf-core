8824dc4d48ab98020f2e6b56dde311b2886162b5
Kevyn <Kevyn@192.168.1.100>
2016-09-29 02:22:42 -0400

    Deleted commented out code

--- src/edu/njit/cs/saboc/blu/core/gui/gep/panels/details/optionbuttons/CreateDisjointAbNButton.java
+++ src/edu/njit/cs/saboc/blu/core/gui/gep/panels/details/optionbuttons/CreateDisjointAbNButton.java
@@ -14,76 +14,40 @@ import javax.swing.SwingUtilities;
  * @author Chris O
  */
 public class CreateDisjointAbNButton<T extends Node> extends NodeOptionButton<T> {
-    
+
     private final PartitionedAbNConfiguration config;
     private final DisplayAbNAction<DisjointAbstractionNetwork> displayAbNAction;
-    
+
     public CreateDisjointAbNButton(
             PartitionedAbNConfiguration config,
             DisplayAbNAction<DisjointAbstractionNetwork> displayAbNAction) {
-        
-        super("BluDisjointAbN.png", 
-                String.format("Display disjoint %s", 
+
+        super("BluDisjointAbN.png",
+                String.format("Display disjoint %s",
                         config.getTextConfiguration().getAbNTypeName(false)));
-        
+
         this.config = config;
         this.displayAbNAction = displayAbNAction;
-        
+
         this.addActionListener((ActionEvent ae) -> {
             createAndDisplayDisjointAbNAction();
         });
     }
-    
+
     public final void createAndDisplayDisjointAbNAction() {
         final PartitionedNode node = (PartitionedNode) super.getCurrentNode().get();
-/*
-        Thread loadThread = new Thread(new Runnable() {
-
-            private LoadStatusDialog loadStatusDialog = null;
-            private boolean doLoad = true;
-
-            public void run() {
-
-                loadStatusDialog = LoadStatusDialog.display(null,
-                        String.format("Creating %s Disjoint %s", 
-                                node.getName(),
-                                config.getTextConfiguration().getAbNTypeName(false)),
-                        new LoadStatusDialog.LoadingDialogClosedListener() {
-
-                            @Override
-                            public void dialogClosed() {
-                                doLoad = false;
-                            }
-                        });
-
-                DisjointAbstractionNetwork disjointAbN = config.getDisjointAbstractionNetworkFor(node);
-
-                SwingUtilities.invokeLater(new Runnable() {
-                    public void run() {
-                        if (doLoad) {
-                            displayAbNAction.displayAbstractionNetwork(disjointAbN);
-
-                            loadStatusDialog.setVisible(false);
-                            loadStatusDialog.dispose();
-                        }
-                    }
-                });
-            }
-        });
-
-        loadThread.start();
-        */
-        DisplayNewDisjointAbNAction display = new DisplayNewDisjointAbNAction(config, displayAbNAction, node, String.format("Creating %s Disjoint %s", 
-                                node.getName(),
-                                config.getTextConfiguration().getAbNTypeName(false)));
+       
+        DisplayNewDisjointAbNAction display = new DisplayNewDisjointAbNAction(config, displayAbNAction, node, String.format("Creating %s Disjoint %s",
+                node.getName(),
+                config.getTextConfiguration().getAbNTypeName(false)));
         display.startThread();
 
     }
 
     @Override
     public void setEnabledFor(Node node) {
-        PartitionedNode partitionedNode = (PartitionedNode)node;
-        
+        PartitionedNode partitionedNode = (PartitionedNode) node;
+
         this.setEnabled(partitionedNode.hasOverlappingConcepts());
     }
 }
--- src/edu/njit/cs/saboc/blu/core/gui/gep/panels/details/optionbuttons/CreateTANButton.java
+++ src/edu/njit/cs/saboc/blu/core/gui/gep/panels/details/optionbuttons/CreateTANButton.java
@@ -2,10 +2,8 @@ package edu.njit.cs.saboc.blu.core.gui.gep.panels.details.optionbuttons;
 
 import edu.njit.cs.saboc.blu.core.abn.node.Node;
 import edu.njit.cs.saboc.blu.core.abn.tan.ClusterTribalAbstractionNetwork;
-import edu.njit.cs.saboc.blu.core.gui.dialogs.LoadStatusDialog;
 import edu.njit.cs.saboc.blu.core.gui.listener.DisplayAbNAction;
 import java.awt.event.ActionEvent;
-import javax.swing.SwingUtilities;
 
 /**
  *
@@ -28,35 +26,6 @@ public abstract class CreateTANButton<T extends Node> extends NodeOptionButton<T
     
     public void createAndDisplayTAN() {
         if (getCurrentNode().isPresent()) {
-            /*
-            Thread loadThread = new Thread(new Runnable() {
-
-                private LoadStatusDialog loadStatusDialog = null;
-                private boolean doLoad = true;
-
-                public void run() {
-                    
-                    loadStatusDialog = LoadStatusDialog.display(null,
-                            "Creating Tribal Abstraction Network (TAN)", 
-                            ( ) -> {
-                                doLoad = false;
-                    });
-
-                    ClusterTribalAbstractionNetwork tan = deriveTAN();
-
-                    SwingUtilities.invokeLater(() -> {
-                        if (doLoad) {
-                            displayTAN.displayAbstractionNetwork(tan);
-
-                            loadStatusDialog.setVisible(false);
-                            loadStatusDialog.dispose();
-                        }
-                    });
-                }
-            });
-
-            loadThread.start();
-            */
             DisplayNewTAN display = new DisplayNewTAN(this, this.displayTAN, "Creating Tribal Abstraction Network (TAN)");
             display.startThread();
         }

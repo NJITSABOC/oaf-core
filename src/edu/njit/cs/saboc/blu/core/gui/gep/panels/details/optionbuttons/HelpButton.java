7fdadf2ba5aeb5922be87f8c544aafe9305f6c08
Atsuki Imamura <ai64@njit.edu>
2017-02-20 19:52:28 -0500

    Help Button for Option Panel

--- src/edu/njit/cs/saboc/blu/core/gui/gep/panels/details/optionbuttons/HelpButton.java
+++ src/edu/njit/cs/saboc/blu/core/gui/gep/panels/details/optionbuttons/HelpButton.java
@@ -1,34 +1,58 @@
 package edu.njit.cs.saboc.blu.core.gui.gep.panels.details.optionbuttons;
 
 import edu.njit.cs.saboc.blu.core.abn.node.Node;
+import edu.njit.cs.saboc.blu.core.abn.node.PartitionedNode;
+import edu.njit.cs.saboc.blu.core.abn.node.SinglyRootedNode;
+import edu.njit.cs.saboc.blu.core.gui.gep.panels.configuration.AbNConfiguration;
+import edu.njit.cs.saboc.blu.core.gui.gep.panels.configuration.PartitionedAbNTextConfiguration;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 
 /**
- * Created by atsukiima on 2/12/2017.
+ * Created by Atsuki Imamura on 2/12/2017.
  */
 public class HelpButton<T extends Node> extends NodeOptionButton<T> {
 
-    public HelpButton(){
+    public HelpButton(AbNConfiguration config){
         super("BluHelp.png", "Help");
-        System.out.println("check this out 1111!");
         this.addActionListener((ActionEvent ae) -> {
             SwingUtilities.invokeLater(new Runnable() {
                 public void run() {
-                    System.out.println("check this out!");
-                    displayDetailsWindow();
+                    displayDetailsWindow(config);
                 }
             });
         });
     }
 
-    private void displayDetailsWindow() {
-        System.out.println("check this out 222!");
+    private void displayDetailsWindow(AbNConfiguration config) {
+        String nodeTypeName, nodeHelpDescription;
+        Node curNode = getCurrentNode().get();
+        if(!(curNode instanceof SinglyRootedNode)){
+            nodeTypeName = ((PartitionedAbNTextConfiguration) config.getTextConfiguration()).getContainerTypeName(false);
+            nodeHelpDescription = ((PartitionedAbNTextConfiguration) config.getTextConfiguration()).getContainerHelpDescription((PartitionedNode) curNode);
+        }
+        else{
+            nodeTypeName = config.getTextConfiguration().getNodeTypeName(false);
+            nodeHelpDescription = config.getTextConfiguration().getNodeHelpDescription(curNode);
+        }
         JDialog detailsDialog = new JDialog();
+        detailsDialog.setTitle(String.format("(%s) Help / Description",nodeTypeName));
+        detailsDialog.setModal(true);
         detailsDialog.setSize(700, 600);
+        detailsDialog.setLocationRelativeTo(null);  //sets the location to the center
 
-//        detailsDialog.add(generatorAction.generatePanel());
+        JEditorPane nodeDetailsPane = new JEditorPane();
+        nodeDetailsPane.setContentType("text/html");
+        nodeDetailsPane.setEnabled(true);
+        nodeDetailsPane.setEditable(false);
+        nodeDetailsPane.setFont(nodeDetailsPane.getFont().deriveFont(Font.BOLD, 14));
+        nodeDetailsPane.setText(nodeHelpDescription);
+
+        detailsDialog.add(new JScrollPane(nodeDetailsPane));
+        detailsDialog.setResizable(true);
+
         detailsDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         detailsDialog.setVisible(true);
     }

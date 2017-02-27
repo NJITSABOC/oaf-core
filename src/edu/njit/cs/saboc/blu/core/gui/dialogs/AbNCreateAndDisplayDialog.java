9894bad15bcff435810fe16f238742c8969fc7b6
cro3 <cro3@itc-4214-dt1.njitdm.campus.njit.edu>
2017-02-27 14:16:08 -0500

    Generic create and display dialog (no longer AbN-specific)

--- src/edu/njit/cs/saboc/blu/core/gui/dialogs/AbNCreateAndDisplayDialog.java
+++ src/edu/njit/cs/saboc/blu/core/gui/dialogs/AbNCreateAndDisplayDialog.java
@@ -2,59 +2,16 @@ package edu.njit.cs.saboc.blu.core.gui.dialogs;
 
 import edu.njit.cs.saboc.blu.core.abn.AbstractionNetwork;
 import edu.njit.cs.saboc.blu.core.gui.graphframe.AbNDisplayManager;
-import javax.swing.SwingUtilities;
 
 /**
  *
  * @author Kevyn
  * @param <T>
  */
-public abstract class AbNCreateAndDisplayDialog<T extends AbstractionNetwork> {
-
-    private final AbNDisplayManager displayManager;
-    
-    private final String dialogText;
+public abstract class AbNCreateAndDisplayDialog<T extends AbstractionNetwork> 
+    extends CreateAndDisplayDialog<T> {
 
     public AbNCreateAndDisplayDialog(String text, AbNDisplayManager displayManager) {
-        this.displayManager = displayManager;
-
-        this.dialogText = text;
+        super(text, displayManager);
     }
-    
-    public void createAbN() {
-        
-        Thread loadThread = new Thread(new Runnable() {
-            
-            private LoadStatusDialog loadStatusDialog = null;
-            private boolean loadCancelled = false;
-
-            @Override
-            public void run() {
-
-                loadStatusDialog = LoadStatusDialog.display(null, dialogText, () -> {
-                    loadCancelled = true;
-                });
-
-                T abn = deriveAbN();
-
-                SwingUtilities.invokeLater(() -> {
-                    if (!loadCancelled) {
-                        displayAbN(abn);
-
-                        loadStatusDialog.setVisible(false);
-                        loadStatusDialog.dispose();
-                    }
-                });
-            }
-        });
-
-        loadThread.start();
-    }
-    
-    public AbNDisplayManager getDisplayFrameListener() {
-        return displayManager;
-    }
-
-    protected abstract void displayAbN(T abn);
-    protected abstract T deriveAbN();
 }

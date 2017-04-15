cb61416e9e925589766c0a31002e41beacd5b9d5
chriso <cro3@njit.edu>
2017-04-15 15:15:17 -0400

    Reverted recently opened audit set functionality

--- src/edu/njit/cs/saboc/nat/generic/gui/panels/errorreporting/auditset/AuditSetPanel.java
+++ src/edu/njit/cs/saboc/nat/generic/gui/panels/errorreporting/auditset/AuditSetPanel.java
@@ -9,27 +9,14 @@ import edu.njit.cs.saboc.nat.generic.errorreport.AuditSetLoader;
 import edu.njit.cs.saboc.nat.generic.errorreport.AuditSetLoaderException;
 import edu.njit.cs.saboc.nat.generic.gui.panels.BaseNATPanel;
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.GridLayout;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 
 import java.io.File;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.prefs.Preferences;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
@@ -52,13 +39,6 @@ public class AuditSetPanel<T extends Concept> extends BaseNATPanel<T> {
     private final JButton btnExportAuditSet;
     
     private final JButton btnOpenAuditSet;
-
-    private final JButton btnLoadRecentlyOpened;
-    private final JComboBox recentlyOpenedVersionBox;
-    private ArrayList<File> recentlyOpenedList = new ArrayList<>();
-
-    private final String prefsKey_RecentlyOpenedAuditSet = "Recently Opened Audit Set";
-    private final String defaultJSON = "{}";
     
     public AuditSetPanel(NATBrowserPanel<T> browserPanel, ConceptBrowserDataSource<T> dataSource) {
         super(browserPanel, dataSource);
@@ -96,45 +76,6 @@ public class AuditSetPanel<T extends Concept> extends BaseNATPanel<T> {
             openAuditSet();
         });
         
-        this.btnLoadRecentlyOpened = new JButton("Load Recently Opened");
-        this.recentlyOpenedVersionBox = new JComboBox();
-        this.recentlyOpenedVersionBox.setBackground(Color.WHITE);
-        loadHistory();
-        this.btnLoadRecentlyOpened.addActionListener( (ae) -> {
-            try {
-                File file = recentlyOpenedList.get(recentlyOpenedVersionBox.getSelectedIndex());
-                AuditSet<T> auditSet = AuditSetLoader.<T>createAuditSetFromJSON(file, getDataSource());
-                setCurrentAuditSet(auditSet);
-            } catch (AuditSetLoaderException asle) {
-                asle.printStackTrace();
-                JOptionPane.showMessageDialog(
-                        null,
-                        asle.getMessage(),
-                        "Audit Set Loading Error",
-                        JOptionPane.ERROR_MESSAGE);
-            }
-        });
-
-        JButton clearHistoryBtn = new JButton("Clear History");
-        clearHistoryBtn.addActionListener( (ae) -> {
-            int answer = JOptionPane.showOptionDialog(
-                    null,
-                    "The history of recently opened items will be lost."
-                            + "\nAre you sure to clear history?",
-                    "Recently Opened Files: Clear History?",
-                    JOptionPane.YES_NO_OPTION,
-                    JOptionPane.WARNING_MESSAGE,
-                    null,
-                    new Object[]{"Yes","No"},
-                    "No");
-            switch(answer) {
-                case 0:
-                    eraseHistory();
-                    loadHistory();
-                case 1:
-                default:
-            }
-        });
         
         this.btnExportAuditSet = new JButton("Save As");
         this.btnExportAuditSet.addActionListener( (ae) -> {
@@ -160,9 +101,6 @@ public class AuditSetPanel<T extends Concept> extends BaseNATPanel<T> {
         
         JPanel detailsPanel = new JPanel(new GridLayout(2, 1));
         
-        detailsPanel.add(recentlyOpenedVersionBox);
-        detailsPanel.add(btnLoadRecentlyOpened);
-        detailsPanel.add(clearHistoryBtn);
         detailsPanel.add(namePanel);
         detailsPanel.add(lastSavedPanel);
         
@@ -230,8 +168,6 @@ public class AuditSetPanel<T extends Concept> extends BaseNATPanel<T> {
             try {
                 AuditSet<T> auditSet = AuditSetLoader.<T>createAuditSetFromJSON(idFile.get(), getDataSource());
                 setCurrentAuditSet(auditSet);
-                saveHistory(idFile.get());
-                loadHistory();
             } catch (AuditSetLoaderException asle) {
                 asle.printStackTrace();
             }
@@ -249,8 +185,6 @@ public class AuditSetPanel<T extends Concept> extends BaseNATPanel<T> {
                 
                 if(success) {
                     displayDetails(auditSet);
-                    saveHistory(auditSetFile.get());
-                    loadHistory();
                 } else {
                     JOptionPane.showMessageDialog(getMainPanel().getParentFrame(), 
                             "<html>An error occured when saving the Audit Set."
@@ -258,8 +192,6 @@ public class AuditSetPanel<T extends Concept> extends BaseNATPanel<T> {
                             "Error Saving Audit Set", 
                             JOptionPane.ERROR_MESSAGE);
                 }
-                
-                
             }
         }
     }
@@ -281,142 +213,5 @@ public class AuditSetPanel<T extends Concept> extends BaseNATPanel<T> {
         return Optional.ofNullable(auditSetName);
     }
     
-    
-    private List<Map.Entry<String,Long>> getRecentlyOpened(Class<?> c, String prefsKey) throws ParseException {
-        Preferences prefsRoot = Preferences.userNodeForPackage(c);
-        String recentlyOpenedReleasesListJSON = prefsRoot.get(prefsKey,defaultJSON);
-        System.out.println(recentlyOpenedReleasesListJSON);
-        JSONParser parser = new JSONParser();
-        JSONObject recentlyOpenedReleasesJSONobj = (JSONObject) parser.parse(recentlyOpenedReleasesListJSON);
-        HashMap<String,Long> unsorted = new HashMap<>();
-        recentlyOpenedReleasesJSONobj.keySet().forEach( key -> {
-            unsorted.put((String) key,(Long) recentlyOpenedReleasesJSONobj.get(key));
-        });
-        List<Map.Entry<String,Long>> sortedEntries = new ArrayList<>(unsorted.entrySet());
-        Collections.sort(sortedEntries,
-                (Map.Entry<String,Long> e1, Map.Entry<String,Long> e2) ->
-                        e2.getValue().compareTo(e1.getValue())
-        );
-        return sortedEntries;
-    }
 
-    private ArrayList<File> getHistory(Class<?> c, String prefsKey) throws ParseException {
-        List<Map.Entry<String,Long>> sortedEntries = getRecentlyOpened(c,prefsKey);
-        ArrayList<File> returnList = new ArrayList<>(sortedEntries.size());
-
-        for (Map.Entry<String,Long> entry : sortedEntries){
-            String path = entry.getKey();
-            File temp = new File(path);
-            try {
-                if(temp.exists())
-                    returnList.add(temp);
-            } catch (SecurityException se) {
-                se.printStackTrace();
-                JOptionPane.showMessageDialog(
-                        null,
-                        "One of the recently opened location is not readable - READ ACCESS DENIED."
-                                + "\nLocation path: " + path
-                                + "\nThis location is removed from the recently opened list.",
-                        "File/Directory Read Error",
-                        JOptionPane.ERROR_MESSAGE);
-            }
-        }
-        return returnList;
-    }
-
-    private void eraseHistory(){
-        eraseHistory(this.getClass(),prefsKey_RecentlyOpenedAuditSet);
-    }
-
-    private void eraseHistory(Class<?> c, String prefsKey){
-        Preferences prefsRoot = Preferences.userNodeForPackage(c);
-        prefsRoot.remove(prefsKey);
-    }
-
-    private void loadHistory(){
-        try {
-            recentlyOpenedVersionBox.removeAllItems();
-        }catch(NullPointerException e){
-            System.out.println("NullPointerException!!");
-        }
-        recentlyOpenedList.clear();
-        try{
-            recentlyOpenedList = getHistory(this.getClass(), prefsKey_RecentlyOpenedAuditSet);
-            recentlyOpenedList.forEach( file -> {
-                recentlyOpenedVersionBox.addItem(file.getName());
-            });
-        }
-        catch(ParseException pe) {
-            pe.printStackTrace();
-            JOptionPane.showMessageDialog(
-                    null,
-                    "Reading the list of recently opened files failed - PARSE ERROR",
-                    "Recently Opened Files: Read Failed",
-                    JOptionPane.ERROR_MESSAGE);
-        }
-        catch(ClassCastException|NumberFormatException e){
-            e.printStackTrace();
-            int answer = JOptionPane.showOptionDialog(
-                    null,
-                    "Reading the list of recently opened files failed - DATA ERROR"
-                            + "\nHistory data seems corrupted.\nReset history?",
-                    "Recently Opened Files: Read Failed",
-                    JOptionPane.YES_NO_OPTION,
-                    JOptionPane.ERROR_MESSAGE,
-                    null,
-                    new Object[]{"Yes","No"},
-                    "No");
-            switch(answer) {
-                case 0:
-                    eraseHistory();
-                case 1:
-                default:
-            }
-        }
-        catch(Exception e){
-            e.printStackTrace();
-            int answer = JOptionPane.showOptionDialog(
-                    null,
-                    "Reading the list of recently opened files failed - UNKNOWN ERROR"
-                            + "\nReset history?",
-                    "Recently Opened Files: Read Failed",
-                    JOptionPane.YES_NO_OPTION,
-                    JOptionPane.ERROR_MESSAGE,
-                    null,
-                    new Object[]{"Yes","No"},
-                    "No");
-            switch(answer) {
-                case 0:
-                    eraseHistory();
-                case 1:
-                default:
-            }
-        }
-        if(recentlyOpenedList.isEmpty()){
-            recentlyOpenedVersionBox.addItem("NO History");
-            btnLoadRecentlyOpened.setEnabled(false);
-        }
-        else{
-            btnLoadRecentlyOpened.setEnabled(true);
-        }
-    }
-
-    private void saveHistory(File selectedFile){
-        Preferences prefsRoot = Preferences.userNodeForPackage(this.getClass());
-        String recentlyOpenedReleasesListJSON = prefsRoot.get(prefsKey_RecentlyOpenedAuditSet,defaultJSON);
-        System.out.println(recentlyOpenedReleasesListJSON);
-        JSONParser parser = new JSONParser();
-        try{
-            JSONObject recentlyOpenedReleasesJSONObj = (JSONObject) parser.parse(recentlyOpenedReleasesListJSON);
-            recentlyOpenedReleasesJSONObj.put(selectedFile.getAbsolutePath(),new Date().getTime());
-            prefsRoot.put(prefsKey_RecentlyOpenedAuditSet, JSONValue.toJSONString(recentlyOpenedReleasesJSONObj));
-        }catch(ParseException pe){
-            pe.printStackTrace();
-            JOptionPane.showMessageDialog(
-                    null,
-                    "Saving the list of recently opened files failed - PARSE ERROR",
-                    "Recently Opened Files: Save Failed",
-                    JOptionPane.ERROR_MESSAGE);
-        }
-    }
 }

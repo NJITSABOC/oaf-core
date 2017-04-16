21e0ec64869a0a8e16615e0a176473f65afade08
chriso <cro3@njit.edu>
2017-04-16 15:34:57 -0400

    Added summary text for target abn

--- src/edu/njit/cs/saboc/blu/core/gui/gep/panels/details/targetbased/configuration/TargetAbNTextConfiguration.java
+++ src/edu/njit/cs/saboc/blu/core/gui/gep/panels/details/targetbased/configuration/TargetAbNTextConfiguration.java
@@ -4,6 +4,9 @@ import edu.njit.cs.saboc.blu.core.abn.targetbased.TargetAbstractionNetwork;
 import edu.njit.cs.saboc.blu.core.abn.targetbased.TargetGroup;
 import edu.njit.cs.saboc.blu.core.gui.gep.panels.configuration.AbNTextConfiguration;
 import edu.njit.cs.saboc.blu.core.gui.gep.panels.configuration.OntologyEntityNameConfiguration;
+import edu.njit.cs.saboc.blu.core.ontology.Concept;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -11,11 +14,11 @@ import edu.njit.cs.saboc.blu.core.gui.gep.panels.configuration.OntologyEntityNam
  */
 public abstract class TargetAbNTextConfiguration extends AbNTextConfiguration<TargetGroup> {
     
-    private final TargetAbstractionNetwork targetAbN;
+    private final TargetAbstractionNetwork<?> targetAbN;
     
     public TargetAbNTextConfiguration(
             OntologyEntityNameConfiguration ontologyEntityNameConfig, 
-            TargetAbstractionNetwork taxonomy) {
+            TargetAbstractionNetwork<?> taxonomy) {
         
         super(ontologyEntityNameConfig);
         
@@ -33,11 +36,43 @@ public abstract class TargetAbNTextConfiguration extends AbNTextConfiguration<Ta
 
     @Override
     public String getAbNSummary() {
-       return "[TARGET ABN SUMMARY TEXT]";
+        
+        String desc = "<html>The <b><abnName></b> target abstraction network summarizes the "
+                + "<selectedPropertyName> <propertyTypeName count=2> between the "
+                + "<sourceHierarchyName> hierarchy and <targetHierarchyName> hierarchy."
+                + "<p>"
+                + "A total of <font color = 'RED'><targetConceptCount></font> target <conceptTypeName count=<targetConceptCount>> "
+                + "are summarized by <font color = 'RED'><targetGroupCount></font> <nodeTypeName count=<targetGroupCount>>."
+                + "<p>"
+                + "A total of <font color = 'RED'><sourceConceptCount></font> source <conceptTypeName count=<sourceConceptCount>> "
+                + "have <propertyTypeName count=2> to these targets.";
+        
+        Set<Concept> sourceConcepts = new HashSet<>();
+        
+        Set<Concept> targetConcepts = new HashSet<>();
+        
+        targetAbN.getTargetGroups().forEach( (targetGroup) -> {
+            targetConcepts.addAll(targetGroup.getIncomingRelationshipTargets());
+            sourceConcepts.addAll(targetGroup.getIncomingRelationshipSources());
+        });
+        
+        desc = desc.replaceAll("<abnName>", this.getAbNName());
+        desc = desc.replaceAll("<selectedPropertyName>", targetAbN.getDerivation().getPropertyType().getName());
+        desc = desc.replaceAll("<sourceHierarchyName>", targetAbN.getDerivation().getSourceHierarchyRoot().getName());
+        desc = desc.replaceAll("<targetHierarchyName>", targetAbN.getDerivation().getTargetHierarchyRoot().getName());
+        
+        desc = desc.replaceAll("<targetGroupCount>", Integer.toString(targetAbN.getNodes().size()));
+        desc = desc.replaceAll("<targetConceptCount>", Integer.toString(targetConcepts.size()));
+        desc = desc.replaceAll("<sourceConceptCount>", Integer.toString(sourceConcepts.size()));
+        
+        return desc;
     }
     
     @Override
     public String getAbNHelpDescription() {
+        
+        
+        
         return "[TARGET ABN HELP DESCRIPTION]";
     }
 
@@ -61,6 +96,12 @@ public abstract class TargetAbNTextConfiguration extends AbNTextConfiguration<Ta
 
     @Override
     public String getNodeHelpDescription(TargetGroup node) {
-        return "";
+        
+        String descriptionStr = ""
+                + "<html>A <b>target group</b> summarizes a subhierarchy of <conceptTypeName count=2> "
+                + "that are targets of <propertyTypeName> relationships. The target group is named after "
+                + "the lowest common ancestor that is not a target but has descendents that are targets.";
+        
+        return descriptionStr;
     }
 }

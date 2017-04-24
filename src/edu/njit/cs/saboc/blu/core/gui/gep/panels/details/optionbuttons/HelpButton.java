22475b23b5cc861868d97d78e675f9efbcf2c620
cro3 <cro3@itc-4214-dt1.njitdm.campus.njit.edu>
2017-04-24 17:25:10 -0400

    Small refactor of node help button

--- src/edu/njit/cs/saboc/blu/core/gui/gep/panels/details/optionbuttons/HelpButton.java
+++ src/edu/njit/cs/saboc/blu/core/gui/gep/panels/details/optionbuttons/HelpButton.java
@@ -8,20 +8,20 @@ import edu.njit.cs.saboc.blu.core.gui.gep.panels.configuration.PartitionedAbNTex
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 
 /**
  * Created by Atsuki Imamura on 2/12/2017.
+ * 
+ * @param <T>
  */
 public class HelpButton<T extends Node> extends NodeOptionButton<T> {
 
     public HelpButton(AbNConfiguration config){
         super("BluHelp.png", "Help");
-        this.addActionListener((ActionEvent ae) -> {
-            SwingUtilities.invokeLater(new Runnable() {
-                public void run() {
-                    displayDetailsWindow(config);
-                }
+        
+        this.addActionListener((ae) -> {
+            SwingUtilities.invokeLater(() -> {
+                displayDetailsWindow(config);
             });
         });
     }
@@ -29,18 +29,19 @@ public class HelpButton<T extends Node> extends NodeOptionButton<T> {
     private void displayDetailsWindow(AbNConfiguration config) {
         String nodeTypeName, nodeHelpDescription;
         Node curNode = getCurrentNode().get();
-        if(!(curNode instanceof SinglyRootedNode)){
+        
+        if (curNode instanceof SinglyRootedNode) {
+            nodeTypeName = config.getTextConfiguration().getNodeTypeName(false);
+            nodeHelpDescription = config.getTextConfiguration().getNodeHelpDescription(curNode);
+        } else {
             nodeTypeName = ((PartitionedAbNTextConfiguration) config.getTextConfiguration()).getContainerTypeName(false);
             nodeHelpDescription = ((PartitionedAbNTextConfiguration) config.getTextConfiguration()).getContainerHelpDescription((PartitionedNode) curNode);
         }
-        else{
-            nodeTypeName = config.getTextConfiguration().getNodeTypeName(false);
-            nodeHelpDescription = config.getTextConfiguration().getNodeHelpDescription(curNode);
-        }
+
         JDialog detailsDialog = new JDialog();
         detailsDialog.setTitle(String.format("(%s) Help / Description",nodeTypeName));
         detailsDialog.setModal(true);
-        detailsDialog.setSize(700, 600);
+        detailsDialog.setSize(400, 400);
         detailsDialog.setLocationRelativeTo(null);  //sets the location to the center
 
         JEditorPane nodeDetailsPane = new JEditorPane();
